66adb8f65a8e669d2674ffeb91c00341e827fdc9
Jules PRUGNIAUD <54936081+AbbeSoury@users.noreply.github.com>
2021-06-09 08:37:38 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 import tabJoursEnOrdre from "./Utilitaire/gestionTemps.js";
 
 const CLEFAPI = '59e69651fc51857ecad3510909796350';
+const CLEFAPIGEOLOC = 'c80b945a7d0eb5eae7fc665313c53246';
+
 let resultatsAPI;
+let resultatsAPIPos;
 
 const temps = document.querySelector('.temps');
 const temperature = document.querySelector('.temperature');
@@ -11,19 +14,42 @@ const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
 const joursDiv = document.querySelectorAll('.jour-prevision-nom');
 const tempJoursDiv = document.querySelectorAll('.jour-prevision-temp');
 const imgIcone = document.querySelector('.logo-meteo');
-const chargementContainer = document.querySelector('.overlay-icone-chargement');
+// const chargementContainer = document.querySelector('.overlay-choix-ville');
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position => {
 
-        //console.log(position);
-        let long = position.coords.longitude
-        let lat = position.coords.latitude
-        AppelAPI(long,lat)
+// if(navigator.geolocation){
+//     navigator.geolocation.getCurrentPosition(position => {
 
-    }, ()=> {
-        alert(`Vous avez refusé la géolocalisation, l'application ne peut pas fonctionner, veuillez l'activer.!`)
+//         //console.log(position);
+//         let long = position.coords.longitude
+//         let lat = position.coords.latitude
+//         AppelAPI(long,lat)
+
+//     }, ()=> {
+//         alert(`Vous avez refusé la géolocalisation, l'application ne peut pas fonctionner, veuillez l'activer.!`)
+//     })
+// }
+var  name =  window.prompt('Veuillez choisir une ville')
+
+if(name != null ) {
+    GetCityPos(name)
+}
+
+function GetCityPos(name){
+
+    fetch(`http://api.positionstack.com/v1/forward?access_key=${CLEFAPIGEOLOC}&query=${name}`)
+    .then((reponse) => {
+        return reponse.json();
     })
+    .then((data2) => {
+        //console.log(data2);
+        resultatsAPIPos = data2 ;
+
+        let long = resultatsAPIPos.data[0].longitude
+        let lat = resultatsAPIPos.data[0].latitude
+
+        AppelAPI(long,lat)
+})
 }
 
 function AppelAPI(long,lat){
@@ -39,7 +65,7 @@ function AppelAPI(long,lat){
 
         temps.innerText = resultatsAPI.current.weather[0].description;
         temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`;
-        localisation.innerText = resultatsAPI.timezone;
+        localisation.innerText = resultatsAPIPos.data[0].label;
 
 
         // les heures, par tranche de trois, avec leur température.
@@ -85,6 +111,5 @@ function AppelAPI(long,lat){
         }
 
 
-        chargementContainer.classList.add('disparition');
     })
 }
